7da50ed1887b97a4f57ca59df6ba34f67e39b1a8
Quadas <musadaq23@hotmail.com>
2023-01-12 18:53:23 -0500

    created new feature editor for youtubesearch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+YoutubeSearch();
+
 function modal(){
 }
 
@@ -6,6 +8,26 @@ function youtubeTrailer() {
 
 }
 
-function youtubeSearchResults() {
+let MovieTitle ;
 
+
+function YoutubeSearch() {  
+
+const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '6dd73d4db3msh0923a1f1b627dd8p108117jsnbe85780d96b2',
+            'X-RapidAPI-Host': 'youtube-search-results.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://youtube-search-results.p.rapidapi.com/youtube-search/?q=david', options) 
+        .then((response) => {
+        console.log(response);
+        return response.json()
+    }).then((data) => {
+        console.log(data);
+    }).catch((error) => { 
+        console.log(error);    
+    })
 }
