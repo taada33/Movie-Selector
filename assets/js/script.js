d06f34345be8e15b11763c7e541a97d3fd5519a9
Quadas <musadaq23@hotmail.com>
2023-01-16 10:57:58 -0500

    saved js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,27 +68,3 @@ function tmdb () {
 function youtubeTrailer() {
 
 }
-
-
-
-
-function YoutubeSearch() {  
-
-const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '6dd73d4db3msh0923a1f1b627dd8p108117jsnbe85780d96b2',
-            'X-RapidAPI-Host': 'youtube-search-results.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://youtube-search-results.p.rapidapi.com/youtube-search/?q=david', options) 
-        .then((response) => {
-        console.log(response);
-        return response.json()
-    }).then((data) => {
-        console.log(data);
-    }).catch((error) => { 
-        console.log(error);    
-    })
-}
