a5f63cb7d4b70a9bdde9c2e552d40ae20a7c0f2b
Thomas <taadamson33@gmail.com>
2023-01-16 20:50:10 -0500

    mixed code from contributors

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,8 @@ submitBtn.onclick = function(){
     // console.log("title: " + title + " Actor: " + actor + " Genre: " + genre)
 
     //run fetch functions here
+    // YoutubeSearch(title);
+    searchForMovies(title);
 
 }
 
@@ -55,10 +57,57 @@ function localStorage(){
 }
 
 
-function tmdb () {
 
+const api_key = '3abe1bfe865a6d503f549804367c9123';
+
+function searchForMovies(userInput) {
+  var apiUrl = `https://api.themoviedb.org/3/search/movie?query=${userInput}&api_key=${api_key}&language=en-US&page=1&include_adult=false`
+
+  fetch(apiUrl)
+    .then(function (response) {
+      // convert json to regular Javascript object
+      return response.json();
+    })
+    .then(function (data) {
+      console.log('Search results are', data)
+      var { results } = data; // destructuring results from data;
+      var { id } = results[0];
+      getReview(id);
+      // run displayReviews function here
+      // displayReviews();
+    });
+};
+
+
+
+
+// this is also a function definition - tmdb
+// which hasn't been invoked yet.
+function getReview(Movie_id) {
+  var apiUrl = `https://api.themoviedb.org/3/movie/${Movie_id}/reviews?api_key=${api_key}`
+
+  fetch(apiUrl)
+    .then(function (response) {
+      // convert json to regular Javascript object
+      return response.json();
+    })
+    .then(function (data) {
+      console.log('Movie reviews are', data);
+      var { results } = data; // destructuring results from data;
+      // what do you want to do with your array of review objects?
+      // maybe you run displayReviews function here
+      displayReviews(results);
+    });
+
+};
+
+function displayReviews(movieReviews) {
+  // loop through your array and show the reviews to the screen
+  for (let i = 0; i < movieReviews.length; i++) {
+    console.log(movieReviews[i]);
+    // and so on...
+  }
 }
-
 
 
 const options = {
@@ -69,14 +118,16 @@ const options = {
   }
 };
   
-function YoutubeSearch() {
-
+function YoutubeSearch(title) {
+title = title + " trailer"
 const url = `https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${encodeURIComponent(title)}`
 fetch(url, options) 
   .then(response => {
 return response.json();  
 }).then(data => {
   console.log(data);
+  let dataArray = [data];
+  createElements(dataArray);
 })
 .catch(error => console.log(error))
 
@@ -104,7 +155,7 @@ function createElements(objArray){
     }
 
     videoEl = document.createElement('iframe');
-    videoEl.src = trailerArray[0].url;
+    videoEl.src = "https://www.youtube.com/embed/" + trailerArray[0].id;
     videoEl.width = "420";
     videoEl.height= "345";
 
@@ -124,7 +175,7 @@ function createElements(objArray){
         if(index < 0){
             index = trailerArray.length - 1;
         }
-        videoEl.src = trailerArray[index].url;
+        videoEl.src = "https://www.youtube.com/embed/" + trailerArray[index].id;
         videoTitleEl.textContent = trailerArray[index].title;
     });
 
@@ -139,7 +190,7 @@ function createElements(objArray){
         if(index > trailerArray.length -1){
             index = 0;
         }
-        videoEl.src = trailerArray[index].url;
+        videoEl.src = "https://www.youtube.com/embed/" + trailerArray[index].id;
         videoTitleEl.textContent = trailerArray[index].title;
     });
 
