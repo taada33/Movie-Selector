cf6401c76a55a26d5149560ee71b508fa5b003b6
Jennyzzhh <zzh19980929@gmail.com>
2023-01-18 20:01:34 -0500

    make changes to button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,4 @@
-<<<<<<< HEAD
-function modal() {}
-=======
-
-let titleEl = document.querySelector('.movie-title');
-let descriptionEl = document.querySelector('.description-other');
-let trailerEl = document.querySelector('.trailor-div');
-let reviewEl = document.querySelector('.review-container')
-
-trailerEl.style.display = 'none';
-
-let previousVideoBtn = document.createElement("button");
-previousVideoBtn.innerHTML = "Previous";
-let nextVideoBtn = document.createElement("button");
-nextVideoBtn.innerHTML = "Next";
-
-videoTitleEl = document.createElement('h3');
-
-videoEl = document.createElement('iframe');
-videoEl.allow = "fullscreen";
-videoEl.width = "420";
-videoEl.height= "345";
-
-trailerEl.appendChild(videoEl);
-trailerEl.appendChild(previousVideoBtn);
-trailerEl.appendChild(nextVideoBtn);
-trailerEl.appendChild(videoTitleEl);
-
-let trailerArray = [];
-let dataArray = [];
-
-previousVideoBtn.addEventListener('click', function(){
-  let index;
-  for(let i = 0; i < trailerArray.length; i++){
-      if(videoEl.src === "https://www.youtube.com/embed/" + trailerArray[i].id){
-         index = i; 
-      }
-  }
-  index--;
-  if(index < 0){
-      index = trailerArray.length - 1;
-  }
-  videoEl.src = "https://www.youtube.com/embed/" + trailerArray[index].id;
-  videoTitleEl.textContent = trailerArray[index].title;
-});
-
-nextVideoBtn.addEventListener('click', function(){
-  let index;
-  for(let i = 0; i < trailerArray.length; i++){
-      if(videoEl.src === "https://www.youtube.com/embed/" + trailerArray[i].id){
-         index = i; 
-      }
-  }
-  index++;
-  if(index > trailerArray.length -1){
-      index = 0;
-  }
-  videoEl.src = "https://www.youtube.com/embed/" + trailerArray[index].id;
-  videoTitleEl.textContent = trailerArray[index].title;
-});
-
->>>>>>> 609390d810d91905388385f7cd616f9618b922db
+function modal() { }
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -76,29 +15,29 @@ let genreInputEl = document.getElementById("genreInput");
 let submitBtn = document.getElementById("submitBtn");
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
     modal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 };
 
-submitBtn.onclick = function() {
+submitBtn.onclick = function () {
     event.preventDefault();
 
     //parameters for fetch urls
-    let title = titleInputEl.value;
-    let actor = actorInputEl.value;
-    let genre = genreInputEl.value;
+    title = titleInputEl.value;
+    actor = actorInputEl.value;
+    genre = genreInputEl.value;
 
     titleInputEl.value = "";
     actorInputEl.value = "";
@@ -109,12 +48,8 @@ submitBtn.onclick = function() {
     // console.log("title: " + title + " Actor: " + actor + " Genre: " + genre)
 
     //run fetch functions here
-<<<<<<< HEAD
     localStorageData();
     YoutubeSearch(title);
-=======
-    trailerArray = [];
->>>>>>> 609390d810d91905388385f7cd616f9618b922db
     searchForMovies(title);
 };
 
@@ -132,34 +67,26 @@ function localStorageData() {
             li.setAttribute("data-index", i);
 
             var button = document.createElement("button");
-            button.textContent = "Reviewed ✔️";
+            button.textContent = "Watched ✔️";
 
-<<<<<<< HEAD
+            function tgl(){
+
+
+                if (button.textContent == "Watched ✔️") {
+                    button.textContent  = "Not Watched";
+                }
+
+                else if(button.textContent  == "Not Watched") {
+                    button.textContent  = "Watched ✔️";
+                }
+            }
+            
+            button.addEventListener("click",tgl)
+
             li.appendChild(button);
             historyList.appendChild(li);
         }
     }
-=======
-function searchForMovies(userInput) {
-  var apiUrl = `https://api.themoviedb.org/3/search/movie?query=${userInput}&api_key=${api_key}&language=en-US&page=1&include_adult=false`
-
-  fetch(apiUrl)
-    .then(function (response) {
-      // convert json to regular Javascript object
-      return response.json();
-    })
-    .then(function (data) {
-      var { results } = data; // destructuring results from data;
-      var { id } = results[0];
-      dataArray = [results[0]];
-      getReview(id);
-
-      
-      // run displayReviews function here
-      // displayReviews();
-    });
-};
->>>>>>> 609390d810d91905388385f7cd616f9618b922db
 
     // This function is being called below and will run when the page loads.
     function init() {
@@ -199,63 +126,40 @@ function searchForMovies(userInput) {
     var apiUrl = `https://api.themoviedb.org/3/search/movie?query=${userInput}&api_key=${api_key}&language=en-US&page=1&include_adult=false`;
 
     fetch(apiUrl)
-        .then(function(response) {
+        .then(function (response) {
             // convert json to regular Javascript object
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
             console.log("Search results are", data);
             var { results } = data; // destructuring results from data;
             var { id } = results[0];
             getReview(id);
-            // run displayReviews function here
-            // displayReviews();
+
         });
 }
 
-// this is also a function definition - tmdb
-// which hasn't been invoked yet.
+
 function getReview(Movie_id) {
-<<<<<<< HEAD
     var apiUrl = `https://api.themoviedb.org/3/movie/${Movie_id}/reviews?api_key=${api_key}`;
 
     fetch(apiUrl)
-        .then(function(response) {
+        .then(function (response) {
             // convert json to regular Javascript object
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
             console.log("Movie reviews are", data);
-            var { results } = data; // destructuring results from data;
-            // what do you want to do with your array of review objects?
-            // maybe you run displayReviews function here
-            displayReviews(results);
+            var { results } = data;
+            displayReviews(results); 
         });
 }
-=======
-  var apiUrl = `https://api.themoviedb.org/3/movie/${Movie_id}/reviews?api_key=${api_key}`
-
-  fetch(apiUrl)
-    .then(function (response) {
-      // convert json to regular Javascript object
-      return response.json();
-    })
-    .then(function (data) {
-      var { results } = data; // destructuring results from data;
-      dataArray.push(results); 
-      YoutubeSearch(dataArray[0].original_title);
-      // maybe you run displayReviews function here
-      // displayReviews(results);
-    });
-
-};
->>>>>>> 609390d810d91905388385f7cd616f9618b922db
 
 function displayReviews(movieReviews) {
-    // loop through your array and show the reviews to the screen
+
     for (let i = 0; i < movieReviews.length; i++) {
-        console.log(movieReviews[i]);
-        // and so on...
+        console.log(movieReviews[i]); //create element and append to page
+
     }
 }
 
@@ -268,11 +172,10 @@ const options = {
 };
 
 function YoutubeSearch(title) {
-<<<<<<< HEAD
     title = title + " trailer";
     const url = `https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${encodeURIComponent(
-    title
-  )}`;
+        title
+    )}`;
     fetch(url, options)
         .then((response) => {
             return response.json();
@@ -302,35 +205,9 @@ function YoutubeSearch(title) {
                 };
                 //may need to assign first element in array differently
                 trailerArray.push(videoObj);
-=======
-title = title + " trailer"
-const url = `https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${encodeURIComponent(title)}`
-fetch(url, options) 
-  .then(response => {
-return response.json();  
-}).then(data => {
-  dataArray.push(data);
-  createElements(dataArray);
-})
-.catch(error => console.log(error))
-
-//takes array of fetch data objects from tmdb, utelly, and youtubesearch
-function createElements(objArray){
-  trailerEl.style.display = 'block';
-  console.log(objArray);
-    //working with youtube search results object
-    //creates array of videos filtered from non-video types
-    for(let i = 0; i < objArray[2].items.length; i++){
-        if(objArray[2].items[i].type === "video"){
-            let videoObj = {
-                title: objArray[2].items[i].title,
-                url: objArray[2].items[i].url,
-                id: objArray[2].items[i].id
->>>>>>> 609390d810d91905388385f7cd616f9618b922db
             }
         }
 
-<<<<<<< HEAD
         videoEl = document.createElement("iframe");
         videoEl.src = "https://www.youtube.com/embed/" + trailerArray[0].id;
         videoEl.width = "420";
@@ -341,7 +218,7 @@ function createElements(objArray){
         let nextVideoBtn = document.createElement("button");
         nextVideoBtn.innerHTML = "Next";
 
-        previousVideoBtn.addEventListener("click", function() {
+        previousVideoBtn.addEventListener("click", function () {
             let index;
             for (let i = 0; i < trailerArray.length; i++) {
                 if (videoEl.src === trailerArray[i].url) {
@@ -356,7 +233,7 @@ function createElements(objArray){
             videoTitleEl.textContent = trailerArray[index].title;
         });
 
-        nextVideoBtn.addEventListener("click", function() {
+        nextVideoBtn.addEventListener("click", function () {
             let index;
             for (let i = 0; i < trailerArray.length; i++) {
                 if (videoEl.src === trailerArray[i].url) {
@@ -380,29 +257,4 @@ function createElements(objArray){
         trailerEl.appendChild(nextVideoBtn);
         trailerEl.appendChild(videoTitleEl);
     }
-=======
-    videoEl.src = "https://www.youtube.com/embed/" + trailerArray[2].id;
-    videoTitleEl.textContent = trailerArray[2].title;
-
-    titleEl.textContent = dataArray[0].original_title;
-    descriptionEl.innerHTML = dataArray[0].overview + "<br>" + "Release Date: " + dataArray[0].release_date + "<br>" + "Rating: " + dataArray[0].vote_average
-+ "/10";
-
-    for(let i = 0; i < dataArray[1].length; i++){
-      let reviewCardEl = document.createElement('div');
-      let reviewContentEl = document.createElement('p');
-
-      // console.log(dataArray[1][i].content)
-
-      reviewContentEl.innerHTML = dataArray[1][i].content + "<br>" + "- " + dataArray[1][i].author;
-      
-      reviewCardEl.appendChild(reviewContentEl);
-      reviewEl.appendChild(reviewCardEl);
-    }
-
-
-    
-
-  }
->>>>>>> 609390d810d91905388385f7cd616f9618b922db
 }
